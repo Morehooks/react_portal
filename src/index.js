84155bf032a11a54808d07a1344e8e826aeea3c3
jake <jakelwatson86@gmail.com>
2017-11-18 15:11:25 +0000

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
-import {createStore} from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import todoApp from './reducers'
-import {
-    addTodo,
-    toggleTodo,
-    setVisibilityFilter,
-    visibilityFilters,
-  } from './actions'
+import App from './components/app'
 
 let store  = createStore(
     todoApp,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-//initial state
-console.log(store.getState());
-
-const unsubscribe = store.subscribe(() => 
-    console.log(store.getState())
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
-
-store.dispatch(addTodo('Learn about todos'));
-store.dispatch(addTodo('Learn about Redux'));
-store.dispatch(toggleTodo(0));
-store.dispatch(toggleTodo(1));
-store.dispatch(setVisibilityFilter(visibilityFilters.SHOW_COMPLETED));
-
-unsubscribe();
 
